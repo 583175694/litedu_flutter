c7e24b39a32d7c2649cfc184ae2426dbcbb01c2c
吴少航 <583175694@qq.com>
2020-06-19 16:53:42 +0800

    完成进度条

--- lib/pages/assessment_page.dart
+++ lib/pages/assessment_page.dart
@@ -4,8 +4,12 @@
  * @Date 2020-6-17
  **/
 import 'package:flutter/material.dart';
-import 'package:flutter_seekbar/seekbar/section_text_model.dart';
-import 'package:flutter_seekbar/seekbar/seekbar.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_module/components/screen_fit.dart';
+import 'package:flutter_module/plugins/seekbar_plugin.dart';
+import 'dart:ui' as ui;
+
+import 'package:flutter_seekbar/seekbar/progress_value.dart';
 
 class AssessmentPage extends StatefulWidget {
   @override
@@ -13,305 +17,93 @@ class AssessmentPage extends StatefulWidget {
 }
 
 class AssessmentPageState extends State<AssessmentPage> {
-  List<SectionTextModel> sectionTexts = [];
-
   var v;
+  ui.Image _image;
+  ui.Image _bubble1;
+  ui.Image _bubble2;
+  ui.Image _bubble3;
+  ui.Image _bubble4;
+  List<ui.Image> _imageBubbles;
+  num progressValue = 0.0;
 
   @override
   void initState() {
     super.initState();
-    sectionTexts.add(
-        SectionTextModel(position: 0, text: 'bad', progressColor: Colors.red));
-    sectionTexts.add(SectionTextModel(
-        position: 2, text: 'good', progressColor: Colors.yellow));
-    sectionTexts.add(SectionTextModel(
-        position: 4, text: 'great', progressColor: Colors.green));
+
+    _loadImage('lib/assets/icon_switch.png').then((res) {
+      _image = res;
+    });
+
+    _loadImage('lib/assets/bubble_smile1.png').then((res) {
+      _bubble1 = res;
+    });
+
+    _loadImage('lib/assets/bubble_smile2.png').then((res) {
+      _bubble2 = res;
+    });
+
+    _loadImage('lib/assets/bubble_smile3.png').then((res) {
+      _bubble3 = res;
+    });
+
+    _loadImage('lib/assets/bubble_smile4.png').then((res) {
+      _bubble4 = res;
+    });
+
+    setState(() {});
+  }
+
+  /// 加载图片
+  Future<ui.Image> _loadImage(String path) async {
+    var data = await rootBundle.load(path);
+    var codec = await ui.instantiateImageCodec(data.buffer.asUint8List());
+    var info = await codec.getNextFrame();
+    return info.image;
   }
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1624)
+      ..init(context);
+
+    _imageBubbles = [_bubble1, _bubble2, _bubble3, _bubble4];
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
             padding: EdgeInsets.fromLTRB(0, 80, 0, 0),
             child: Column(
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                          width: 200,
-                          child: SeekBar(
-                              isCanTouch: false,
-                              indicatorRadius: 0.0,
-                              progresseight: 5,
-                              value: 50,
-                              hideBubble: false,
-                              alwaysShowBubble: true,
-                              bubbleRadius: 14,
-                              bubbleColor: Colors.purple,
-                              bubbleTextColor: Colors.white,
-                              bubbleTextSize: 14,
-                              bubbleMargin: 4,
-                              bubbleInCenter: true)),
-                      Text(
-                        "圆角，气泡居中，始终显示气泡",
-                        textDirection: TextDirection.ltr,
-                        maxLines: 1,
-                        style: TextStyle(fontSize: 10),
-                      ),
-                    ],
-                  ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(40, 40, 40, 40),
+                  width: ScreenUtil().setWidth(558),
+                  child: SeekBar(
+                    progressHeight: ScreenUtil().setWidth(32),
+                    value: progressValue,
+                    sectionCount: 4,
+                    sectionRadius: ScreenUtil().setWidth(16),
+                    isRound: true,
+                    showSectionText: false,
+                    progressColor: Color(0xff29D9D6),
+                    backgroundColor: Color(0xffF0F1F4),
+                    sectionTextMarginTop: 2,
+                    sectionDecimal: 0,
+                    sectionTextSize: 14,
+                    hideBubble: false,
+                    afterDragShowSectionText: true,
+                    progressImage: _image,
+                    bubbleImage: _bubble1,
+                    bubbleImages: _imageBubbles,
+                    onValueChanged: (res) {
+                      setState(() {
+                        progressValue = res.value;
+                      });
+                    },
+                  )
                 ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                          padding: EdgeInsets.fromLTRB(0, 0, 0, 6),
-                          width: 200,
-                          child: SeekBar(
-                            progresseight: 5,
-                            value: 20,
-                          )),
-                      Text(
-                        "圆角带指示器",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(fontSize: 10),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                          padding: EdgeInsets.fromLTRB(0, 0, 0, 6),
-                          width: 200,
-                          child: SeekBar(
-                            progresseight: 5,
-                            value: 50,
-                            sectionCount: 5,
-                          )),
-                      Text(
-                        "带间隔带指示器",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(fontSize: 10),
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                          padding: EdgeInsets.fromLTRB(0, 0, 0, 6),
-                          width: 200,
-                          child: SeekBar(
-                            progresseight: 5,
-                            value: 50,
-                            sectionCount: 4,
-                            sectionRadius: 6,
-                            sectionColor: Colors.red,
-                          )),
-                      Text(
-                        "带间隔画间隔的指示器",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(fontSize: 10),
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        textDirection: TextDirection.ltr,
-                        children: <Widget>[
-                          Text(
-                            '-10',
-                            textDirection: TextDirection.ltr,
-                          ),
-                          Container(
-                              margin: EdgeInsets.fromLTRB(10, 0, 10, 4),
-                              width: 200,
-                              child: SeekBar(
-                                  progresseight: 5,
-                                  value: 58,
-                                  min: -10,
-                                  max: 80,
-                                  sectionCount: 4,
-                                  sectionRadius: 6,
-                                  sectionColor: Colors.red,
-                                  hideBubble: false,
-                                  alwaysShowBubble: true,
-                                  bubbleRadius: 14,
-                                  bubbleColor: Colors.purple,
-                                  bubbleTextColor: Colors.white,
-                                  bubbleTextSize: 14,
-                                  bubbleMargin: 4,
-                                  onValueChanged: (v) {
-                                    print('当前进度：${v.progress} ，当前的取值：${v.value}');
-                                  })),
-                          Text(
-                            '80',
-                            textDirection: TextDirection.ltr,
-                          )
-                        ],
-                      ),
-                      Text(
-                        "带间隔带气泡的指示器，气泡",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(fontSize: 10),
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                          margin: EdgeInsets.fromLTRB(0, 0, 0, 40),
-                          width: 200,
-                          child: SeekBar(
-                            progresseight: 10,
-                            value: 50,
-                            sectionCount: 4,
-                            sectionRadius: 5,
-                            sectionColor: Colors.red,
-                            sectionUnSelecteColor: Colors.red[100],
-                            showSectionText: true,
-                            sectionTextMarginTop: 2,
-                            sectionDecimal: 0,
-                            sectionTextColor: Colors.black,
-                            sectionSelectTextColor: Colors.red,
-                            sectionTextSize: 14,
-                          )),
-                      Text(
-                        "带带刻度的指示器,可设置刻度的样式和选中的刻度的样式",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(fontSize: 10),
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                          margin: EdgeInsets.fromLTRB(0, 0, 0, 40),
-                          width: 200,
-                          child: SeekBar(
-                            progresseight: 10,
-                            value: 50,
-                            sectionCount: 4,
-                            sectionRadius: 5,
-                            sectionColor: Colors.red,
-                            sectionUnSelecteColor: Colors.red[100],
-                            showSectionText: true,
-                            sectionTextMarginTop: 2,
-                            sectionDecimal: 0,
-                            sectionTextColor: Colors.black,
-                            sectionSelectTextColor: Colors.red,
-                            sectionTextSize: 14,
-                            hideBubble: false,
-                            bubbleRadius: 14,
-                            bubbleColor: Colors.purple,
-                            bubbleTextColor: Colors.white,
-                            bubbleTextSize: 14,
-                            bubbleMargin: 4,
-                            afterDragShowSectionText: true,
-                          )),
-                      Text(
-                        "带带刻度的指示器,可设置刻度的样式和选中的刻度的样式，拖拽结束显示刻度值，拖拽开始显示气泡",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(fontSize: 10),
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                          margin: EdgeInsets.fromLTRB(0, 0, 0, 40),
-                          width: 200,
-                          child: SeekBar(
-                            min: -100,
-                            max: 200,
-                            progresseight: 10,
-                            value: 50,
-                            sectionCount: 4,
-                            sectionRadius: 6,
-                            showSectionText: true,
-                            sectionTexts: [],
-                            sectionTextMarginTop: 2,
-                            sectionDecimal: 0,
-                            sectionTextColor: Colors.black,
-                            sectionSelectTextColor: Colors.red,
-                            sectionTextSize: 14,
-                            hideBubble: false,
-                            bubbleRadius: 14,
-                            bubbleColor: Colors.purple,
-                            bubbleTextColor: Colors.white,
-                            bubbleTextSize: 14,
-                            bubbleMargin: 4,
-                            afterDragShowSectionText: true,
-                          )),
-                      Text(
-                        "自定义刻度值显示，带带刻度的指示器,可设置刻度的样式和选中的刻度的样式，拖拽结束显示刻度值，拖拽开始显示气泡",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(fontSize: 10),
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                          margin: EdgeInsets.fromLTRB(0, 0, 0, 40),
-                          width: 200,
-                          child: SeekBar(
-                            progresseight: 10,
-                            value: 75,
-                            sectionCount: 4,
-                            sectionRadius: 6,
-                            showSectionText: true,
-                            sectionTexts: sectionTexts,
-                            sectionTextMarginTop: 2,
-                            sectionDecimal: 0,
-                            sectionTextColor: Colors.black,
-                            sectionSelectTextColor: Colors.red,
-                            sectionTextSize: 14,
-                            hideBubble: false,
-                            bubbleRadius: 14,
-                            bubbleColor: Colors.purple,
-                            bubbleTextColor: Colors.white,
-                            bubbleTextSize: 14,
-                            bubbleMargin: 4,
-                            afterDragShowSectionText: true,
-                          )),
-                      Text(
-                        "自定义刻度值显示，带带刻度的指示器,可设置刻度的样式和选中的刻度的样式，拖拽结束显示刻度值，拖拽开始显示气泡",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(fontSize: 10),
-                      )
-                    ],
-                  ),
-                ),
+                Text('${progressValue}')
               ],
-            )
+            ),
         ),
       ),
     );
